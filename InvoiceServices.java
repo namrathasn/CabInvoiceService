972c0fd8eefafce7239e755cbd3a57ecb98df2ab
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-02-03 13:02:08 +0530

    new

---
+++ InvoiceServices.java
@@ -0,0 +1,34 @@
+package com.InvoiceService;
+
+public class InvoiceServices{
+	
+	private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_COSt_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COSt_PER_KILOMETER + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
+
+    public int getNumberOfRides(Ride[] rides) {
+        return rides.length;
+    }
+
+    public double calculateAverageRideCost(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+
+        return totalFare / rides.length;
+    }
+}
